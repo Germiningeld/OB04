8c0ca0738ba48de3f0fc8de713cd35976fac08d4
Germiningeld <germiningeld@gmail.com>
2024-10-08 19:45:51 +0300

    step 4

--- main.py
+++ main.py
@@ -4,25 +4,50 @@ class Fighter():
 
     def __init__(self, weapon):
         self.weapon = weapon
+        print(f'Боец берет {weapon.name}')
 
     def change_weapon(self, new_weapon):
+        print(f'Боец меняет {self.weapon.name} на {new_weapon.name}')
         self.weapon = new_weapon
+
+    def attack(self):
+        self.weapon.attack()
 
 
 class Monster():
-    pass
+
+    def dead(self):
+        print(f'Монстр побежден')
 
 class Weapon(ABC):
+    def __init__(self):
+        self.name = ''
+
     @abstractmethod
     def attack(self):
         pass
 
 class Sword(Weapon):
+    def __init__(self):
+        self.name = 'меч'
 
     def attack(self):
-        pass
+        print(f'Боец наносит удар мечом')
 
 class Bow(Weapon):
+    def __init__(self):
+        self.name = 'лук'
 
     def attack(self):
-        pass
+        print(f'Боец стреляет из лука')
+
+sword = Sword()
+bow = Bow()
+fighter = Fighter(sword)
+monster = Monster()
+
+fighter.attack()
+monster.dead()
+fighter.change_weapon(bow)
+fighter.attack()
+monster.dead()
